1e2b908b8b53fdadddd00362bd762e79858a597e
Yunxing Dai <yunxing@cloudwalk.me>
2015-05-31 22:02:53 -0700

    Add import for gf-complete

--- cgo_flags.go
+++ cgo_flags.go
@@ -1,5 +1,7 @@
 package jerasure
 
+import _ "github.com/cfs/gf-complete"
+// #cgo CXXFLAGS: -std=c++11
 // #cgo CPPFLAGS: -I../gf-complete/internal/include
 // #cgo CPPFLAGS: -Iinternal/include
 // #cgo darwin LDFLAGS: -Wl,-undefined -Wl,dynamic_lookup
